94239b52c8c0749daa521ee050290c6a81f472ed
Greg L. Turnquist <gturnquist@vmware.com>
2022-11-27 07:07:30 -0600

    Upgrade maven wrapper.

--- ch5/src/test/java/com/springbootlearning/learningspringboot3/SecurityBasedTest.java
+++ ch5/src/test/java/com/springbootlearning/learningspringboot3/SecurityBasedTest.java
@@ -8,15 +8,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-//@WebMvcTest(controllers = HomeController.class)
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(controllers = HomeController.class)
 public class SecurityBasedTest {
 
   @Autowired MockMvc mvc;
